900b58b2399b11b3213124004bef07be542519f2
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-08-19 22:23:38 +0900

    Create main.go

---
+++ tour/welcome/4/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    fmt.Println("Welcome to the playground!")
+    fmt.Println("The time is", time.Now())
+}
+
+/*
+Welcome to the playground!
+The time is 2009-11-10 23:00:00 +0000 UTC m=+0.000000001
+*/
